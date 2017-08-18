3236c79f6f72c82b2a9d4498d59c5b499f3d9e18
shamimsjava <shamimsjava@gmail.com>
2017-08-19 00:37:06 +0600

    learn spool from Showkat friend.

--- Chapter6/page18.sql
+++ Chapter6/page18.sql
@@ -1,12 +1,16 @@
-select last_name
+/*select last_name
 from employees
 where employee_id not in(
 	select manager_id
 	from employees
 	where manager_id is not null
-	);
+	);*/
 	
 	
 /*select last_name
 from employees
 where manager_id is null;*/
+
+set pagesize 200;
+select employee_id, first_name, last_name, salary
+from employees;
---
+++ Extra/spool.sql
@@ -0,0 +1,5 @@
+show spool
+spool D:\shamim\shamim.txt
+show spool
+desc employees;
+spool off
